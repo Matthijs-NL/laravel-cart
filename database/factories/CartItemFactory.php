565e01d97ac6be46da426b7a00b12f7979699442
Matthijs Veenendaal <matthijs@digitalself.nl>
2025-10-30 14:40:44 +0000

    added factories and configurable default customer and product models

--- database/factories/CartItemFactory.php
+++ database/factories/CartItemFactory.php
@@ -12,7 +12,7 @@ class CartItemFactory extends Factory
     public function definition()
     {
         return [
-            'cart_id' => Cart::factory(),
+            'cart_id' => Cart::factory()->creat()->id,
             'quantity' => $this->faker->numberBetween(1, 10),
             'itemable_id' => config('laravel-cart.product_model')::factory()->create()->id,
             'itemable_type' => config('laravel-cart.product_model'),
